f225197e0a53f871507dda7de0991604ee763125
Tu Vu <tuvu852@gmail.com>
2023-12-02 08:52:07 -0800

    Clean up ugly code

--- go/2023/day01/trebutchet.go
+++ go/2023/day01/trebutchet.go
@@ -20,19 +20,7 @@ func init() {
 func trebutchetPart1(input string) int {
 	sum := 0
 	for _, line := range strings.Split(input, "\n") {
-		firstDigit, secondDigit := 0, 0
-		for i := 0; i < len(line); i++ {
-			if unicode.IsDigit(rune(line[i])) {
-				firstDigit = int(line[i] - '0')
-				break
-			}
-		}
-		for j := len(line) - 1; j >= 0; j-- {
-			if unicode.IsDigit(rune(line[j])) {
-				secondDigit = int(line[j] - '0')
-				break
-			}
-		}
+		firstDigit, secondDigit := getFirstDigit(line, 1), getSecondDigit(line, 1)
 		sum += firstDigit*10 + secondDigit
 	}
 	return sum
@@ -41,11 +29,59 @@ func trebutchetPart1(input string) int {
 func trebutchetPart2(input string) int {
 	sum := 0
 	for _, line := range strings.Split(input, "\n") {
-		firstDigit, secondDigit := getFirstDigit(line), getSecondDigit(util.ReverseString(line))
-		fmt.Printf("%d%d\n", firstDigit, secondDigit)
+		firstDigit, secondDigit := getFirstDigit(line, 2), getSecondDigit(line, 2)
 		sum += firstDigit*10 + secondDigit
 	}
 	return sum
+}
+
+func getFirstDigit(s string, part int) int {
+	firstDigit, minDigitIndex := 0, len(s)
+	for i := 0; i < len(s); i++ {
+		if unicode.IsDigit(rune(s[i])) {
+			if i < minDigitIndex {
+				firstDigit, minDigitIndex = int(s[i]-'0'), i
+			}
+		}
+	}
+	if part == 2 {
+		// Do a scan of i-letters strings, possible i = 3, 4, 5
+		for i := 3; i <= 5; i++ {
+			for j := 0; j < len(s)-i+1; j++ {
+				if digit, err := digitFromStr(s[j : j+i]); err == nil {
+					if j < minDigitIndex {
+						firstDigit, minDigitIndex = *digit, j
+					}
+				}
+			}
+		}
+	}
+	return firstDigit
+}
+
+func getSecondDigit(s string, part int) int {
+	s = util.ReverseString(s)
+	secondDigit, minDigitIndex := 0, len(s)
+	for i := 0; i < len(s); i++ {
+		if unicode.IsDigit(rune(s[i])) {
+			if i < minDigitIndex {
+				secondDigit, minDigitIndex = int(s[i]-'0'), i
+			}
+		}
+	}
+	if part == 2 {
+		// Do a scan of i-letters strings, possible i = 3, 4, 5
+		for i := 3; i <= 5; i++ {
+			for j := 0; j < len(s)-i+1; j++ {
+				if digit, err := digitFromStr(util.ReverseString(s[j : j+i])); err == nil {
+					if j < minDigitIndex {
+						secondDigit, minDigitIndex = *digit, j
+					}
+				}
+			}
+		}
+	}
+	return secondDigit
 }
 
 // 3, 4, 5
@@ -72,88 +108,4 @@ func digitFromStr(s string) (*int, error) {
 	default:
 		return nil, fmt.Errorf("invalid digit string: %s", s)
 	}
-}
-
-func getFirstDigit(s string) int {
-	firstDigit, minDigitIndex := 0, len(s)
-	for i := 0; i < len(s); i++ {
-		if unicode.IsDigit(rune(s[i])) {
-			if i < minDigitIndex {
-				firstDigit, minDigitIndex = int(s[i]-'0'), i
-			}
-		}
-	}
-	// Do a scan of 3-letters strings
-	if len(s) > 2 {
-		for i := 0; i < len(s)-2; i++ {
-			if digit, err := digitFromStr(s[i : i+3]); err == nil {
-				if i < minDigitIndex {
-					firstDigit, minDigitIndex = *digit, i
-				}
-			}
-		}
-	}
-	// Do a scan of 4-letters strings
-	if len(s) > 3 {
-		for i := 0; i < len(s)-3; i++ {
-			if digit, err := digitFromStr(s[i : i+4]); err == nil {
-				if i < minDigitIndex {
-					firstDigit, minDigitIndex = *digit, i
-				}
-			}
-		}
-	}
-	// Do a scan of 5-letters strings
-	if len(s) > 4 {
-		for i := 0; i < len(s)-4; i++ {
-			if digit, err := digitFromStr(s[i : i+5]); err == nil {
-				if i < minDigitIndex {
-					firstDigit, minDigitIndex = *digit, i
-				}
-			}
-		}
-	}
-	return firstDigit
-}
-
-func getSecondDigit(s string) int {
-	secondDigit, minDigitIndex := 0, len(s)
-	for i := 0; i < len(s); i++ {
-		if unicode.IsDigit(rune(s[i])) {
-			if i < minDigitIndex {
-				secondDigit, minDigitIndex = int(s[i]-'0'), i
-			}
-		}
-	}
-	// Do a scan of 3-letters strings
-	if len(s) > 2 {
-		for i := 0; i < len(s)-2; i++ {
-			if digit, err := digitFromStr(util.ReverseString(s[i : i+3])); err == nil {
-				if i < minDigitIndex {
-					secondDigit, minDigitIndex = *digit, i
-				}
-			}
-		}
-	}
-	// Do a scan of 4-letters strings
-	if len(s) > 3 {
-		for i := 0; i < len(s)-3; i++ {
-			if digit, err := digitFromStr(util.ReverseString(s[i : i+4])); err == nil {
-				if i < minDigitIndex {
-					secondDigit, minDigitIndex = *digit, i
-				}
-			}
-		}
-	}
-	// Do a scan of 5-letters strings
-	if len(s) > 4 {
-		for i := 0; i < len(s)-4; i++ {
-			if digit, err := digitFromStr(util.ReverseString(s[i : i+5])); err == nil {
-				if i < minDigitIndex {
-					secondDigit, minDigitIndex = *digit, i
-				}
-			}
-		}
-	}
-	return secondDigit
 }
